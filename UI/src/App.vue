519de00ff72c3f0ca7a2de737e0a843942bdcbe4
unknown <tst.jorgecardoso@gmail.com>
2025-03-23 17:35:11 -0300

    Criado uma msg de warning que vai ficar visivel durante o desenvolvimento do site

--- UI/src/App.vue
+++ UI/src/App.vue
@@ -1,74 +1,9 @@
 <script setup>
-
-const changeColor = () => {
-  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange', 'indigo', 'teal', 'cyan', 'gray', 'black', 'white'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.documentElement.style.setProperty('--primary-color', randomColor);
-}
+import WarningNewFeature from './components/warnings/WarningNewFeature.vue';
 </script>
 
 <template>
-  <div
-    class="
-      min-w-screen
-      min-h-screen
-      bg-gray-900
-      flex
-      items-center
-      justify-center
-      text-center
-      text-white
-      font-bold
-      space-y-4
-    "
-  >
-    <div>
-      <div
-        class="flex items-center justify-center"
-      >
-        <div
-          class="flex items-center justify-center relative"
-        >
-          <span
-            class="bg-green-500 text-white rounded-full p-2 mr-2"
-          ></span>
-          <span
-            class="bg-green-500 text-white rounded-full p-2 mr-2 animate-ping absolute top-0 right-0"
-          ></span>
-        </div>
-
-        <h1 class="text-4xl">🚧 Em Construção 🚧</h1>
-
-        <div
-            class="flex items-center justify-center relative"
-          >
-            <span
-              class="bg-green-500 text-white rounded-full p-2 ml-2"
-            ></span>
-            <span
-              class="bg-green-500 text-white rounded-full p-2 animate-ping absolute top-0 right-0"
-            ></span>
-          </div>
-      </div>
-      <p class="text-xl">
-        Este recurso ainda está sendo desenvolvido. Volte logo mais para ver a novidade!
-      </p>
-      <p class="text-lg text-gray-400 mt-2">
-        Estamos trabalhando para te entregar a melhor experiência.
-      </p>
-      <div class="mt-4">
-        <span class="text-sm text-gray-500">Voltar à página inicial?</span>
-        <button 
-          class="ml-2 text-blue-500 hover:underline"
-        >
-          Clique aqui
-        </button>
-      </div>
-      <span
-        class="text-xs text-gray-400 mt-2"
-      >
-        Botão acima não funciona. Está apenas por estética. Esse texto também.
-      </span>
-    </div>
+  <div>
+    <WarningNewFeature />
   </div>
 </template>
---
+++ UI/src/components/warnings/WarningNewFeature.vue
@@ -0,0 +1,74 @@
+<script setup>
+
+const changeColor = () => {
+  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange', 'indigo', 'teal', 'cyan', 'gray', 'black', 'white'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.documentElement.style.setProperty('--primary-color', randomColor);
+}
+</script>
+
+<template>
+  <div
+    class="
+      min-w-screen
+      min-h-screen
+      bg-gray-900
+      flex
+      items-center
+      justify-center
+      text-center
+      text-white
+      font-bold
+      space-y-4
+    "
+  >
+    <div>
+      <div
+        class="flex items-center justify-center"
+      >
+        <div
+          class="flex items-center justify-center relative"
+        >
+          <span
+            class="bg-green-500 text-white rounded-full p-2 mr-2"
+          ></span>
+          <span
+            class="bg-green-500 text-white rounded-full p-2 mr-2 animate-ping absolute top-0 right-0"
+          ></span>
+        </div>
+
+        <h1 class="text-4xl">🚧 Em Construção 🚧</h1>
+
+        <div
+            class="flex items-center justify-center relative"
+          >
+            <span
+              class="bg-green-500 text-white rounded-full p-2 ml-2"
+            ></span>
+            <span
+              class="bg-green-500 text-white rounded-full p-2 animate-ping absolute top-0 right-0"
+            ></span>
+          </div>
+      </div>
+      <p class="text-xl">
+        Este recurso ainda está sendo desenvolvido. Volte logo mais para ver a novidade!
+      </p>
+      <p class="text-lg text-gray-400 mt-2">
+        Estamos trabalhando para te entregar a melhor experiência.
+      </p>
+      <div class="mt-4">
+        <span class="text-sm text-gray-500">Voltar à página inicial?</span>
+        <button 
+          class="ml-2 text-blue-500 hover:underline"
+        >
+          Clique aqui
+        </button>
+      </div>
+      <span
+        class="text-xs text-gray-400 mt-2"
+      >
+        Botão acima não funciona. Está apenas por estética. Esse texto também. 😅
+      </span>
+    </div>
+  </div>
+</template>
